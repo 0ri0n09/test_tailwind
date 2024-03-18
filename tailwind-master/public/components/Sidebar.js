d569cf3261de8f41fa783ccdfbff094b14250415
Alexis DAMIEN <alexisdamien-@outlook.fr>
2024-03-18 23:54:24 +0100

    fix/feat : fixes and finish sidebar

--- tailwind-master/public/components/Sidebar.js
+++ tailwind-master/public/components/Sidebar.js
@@ -1,42 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const sidebarButton = document.querySelector("[data-drawer-toggle='default-sidebar']");
-    const sidebar = document.getElementById("default-sidebar");
-    const sidebarOverlay = document.getElementById("sidebar-overlay");
-    const footer = document.querySelector('footer');
-    const closeSidebar = document.getElementById('close-sidebar');
+  const sidebarButton = document.querySelector("[data-drawer-toggle='default-sidebar']");
+  const sidebar = document.getElementById("default-sidebar");
+  const sidebarOverlay = document.getElementById("sidebar-overlay");
+  const footer = document.querySelector('footer');
+  const closeSidebar = document.getElementById('close-sidebar');
+  const modalPromo = document.getElementById('modalPromo');
 
-    closeSidebar.addEventListener("click", toggleSidebar);
-    sidebarButton.addEventListener("click", toggleSidebar);
+  closeSidebar.addEventListener("click", toggleSidebar);
+  sidebarButton.addEventListener("click", toggleSidebar);
 
-    document.addEventListener("click", function (event) {
-        if (!sidebar.contains(event.target) && !sidebarButton.contains(event.target)) {
-            toggleSidebar();
-        }
-    });
-
-    sidebarButton.addEventListener('click', toggleFooter);
-
-    window.addEventListener('resize', adjustSidebarHeight);
-    window.addEventListener('load', adjustSidebarHeight);
-
-    function toggleSidebar() {
-        sidebar.classList.toggle("translate-x-0");
-        sidebarOverlay.classList.toggle("opacity-50");
-        footer.classList.remove("hidden");
+  document.addEventListener("click", function (event) {
+    if (!sidebar.contains(event.target) && !sidebarButton.contains(event.target) && !modalPromo.contains(event.target)) {
+      if (sidebar.classList.contains("translate-x-0")) {
+          toggleSidebar();
+      }
     }
+  });
 
-    function toggleFooter() {
-        footer.classList.toggle('hidden');
-        sidebar.classList.toggle('h-full', footer.classList.contains('hidden'));
+  sidebarButton.addEventListener('click', toggleFooter);
+
+  window.addEventListener('resize', adjustSidebarHeight);
+  window.addEventListener('load', adjustSidebarHeight);
+
+  function toggleSidebar() {
+    sidebar.classList.toggle("translate-x-0");
+    sidebarOverlay.classList.toggle("opacity-50");
+    footer.classList.remove("hidden");
+  }
+
+  function toggleFooter() {
+    footer.classList.toggle('hidden');
+    sidebar.classList.toggle('h-full', footer.classList.contains('hidden'));
+  }
+
+  function adjustSidebarHeight() {
+    if (window.innerWidth >= 768) {
+      var windowHeight = window.innerHeight;
+      var footerHeight = footer.offsetHeight;
+      var adjustedHeight = windowHeight - footerHeight;
+
+      sidebar.style.height = adjustedHeight + 'px';
     }
-
-    function adjustSidebarHeight() {
-        if (window.innerWidth >= 768) {
-            var windowHeight = window.innerHeight;
-            var footerHeight = footer.offsetHeight;
-            var adjustedHeight = windowHeight - footerHeight;
-
-            sidebar.style.height = adjustedHeight + 'px';
-        }
-    }
+  }
 });
