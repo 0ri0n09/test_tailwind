921146b15d3bd73437ed10e3d05fcbfed3c01293
Support Tech PHISOL <support-tech@phi-solutions.net>
2024-03-18 16:00:05 +0100

    multiple fixes

--- tailwind-master/public/components/Sidebar.js
+++ tailwind-master/public/components/Sidebar.js
@@ -3,42 +3,40 @@ document.addEventListener("DOMContentLoaded", function () {
     const sidebar = document.getElementById("default-sidebar");
     const sidebarOverlay = document.getElementById("sidebar-overlay");
     const footer = document.querySelector('footer');
-    //Open
-    sidebarButton.addEventListener("click", function () {
-        sidebar.classList.toggle("translate-x-0");
-        sidebarOverlay.classList.toggle("opacity-50");
-    });
-    //Close
+    const closeSidebar = document.getElementById('close-sidebar');
+
+    closeSidebar.addEventListener("click", toggleSidebar);
+    sidebarButton.addEventListener("click", toggleSidebar);
+
     document.addEventListener("click", function (event) {
         if (!sidebar.contains(event.target) && !sidebarButton.contains(event.target)) {
-            if (sidebarOverlay.classList.contains("opacity-50")) {
-                footer.classList.toggle('hidden');
-            }
-            sidebar.classList.remove("translate-x-0");
-            sidebar.classList.remove("border-r");
-            sidebarOverlay.classList.remove("opacity-50");
+            toggleSidebar();
         }
     });
-    //Hide mobileFooter
+
+    sidebarButton.addEventListener('click', toggleFooter);
+
     window.addEventListener('resize', adjustSidebarHeight);
     window.addEventListener('load', adjustSidebarHeight);
 
-    sidebarButton.addEventListener('click', () => {
-        footer.classList.toggle('hidden');
-
-        if (footer.classList.contains('hidden')) {
-            sidebar.classList.add('h-full');
-        } else {
-            sidebar.classList.remove('h-full');
-        }
-    });
-});
-function adjustSidebarHeight() {
-    if (window.innerWidth >= 768) {
-        var windowHeight = window.innerHeight;
-        var footerHeight = document.querySelector('footer').offsetHeight;
-        var adjustedHeight = windowHeight - footerHeight;
-
-        document.getElementById('default-sidebar').style.height = adjustedHeight + 'px';
+    function toggleSidebar() {
+        sidebar.classList.toggle("translate-x-0");
+        sidebarOverlay.classList.toggle("opacity-50");
+        footer.classList.remove("hidden");
     }
-}
+
+    function toggleFooter() {
+        footer.classList.toggle('hidden');
+        sidebar.classList.toggle('h-full', footer.classList.contains('hidden'));
+    }
+
+    function adjustSidebarHeight() {
+        if (window.innerWidth >= 768) {
+            var windowHeight = window.innerHeight;
+            var footerHeight = footer.offsetHeight;
+            var adjustedHeight = windowHeight - footerHeight;
+
+            sidebar.style.height = adjustedHeight + 'px';
+        }
+    }
+});
